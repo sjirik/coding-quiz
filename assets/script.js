c8a7ae122a8d4e88c01ea2735efe1c6b090a6ea3
Scotty Jirik <ScottyJirik@MacBook-Pro.local>
2023-08-17 17:46:23 -0500

    adding function for button clicks and starting quiz

--- assets/script.js
+++ assets/script.js
@@ -31,6 +31,70 @@ let counterLine;
 let widthValue = 0;
 
 
+next_button.onclick = ()=>{
+    if(que_count < questions.length - 1){ 
+        que_count++; 
+        que_numb++; 
+        showQuetions(que_count); 
+        queCounter(que_numb); 
+        startTimer(timeV); 
+        startTimerLine(widthValue); 
+        timeText.textContent = "Time Left"; 
+        next_button.classList.remove("show"); 
+    }else{
+        showResult(); 
+    }
+}
+
+
+function showQuetions(index){
+    var que_text = document.querySelector(".question_text");
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag; 
+    var option = option_list.querySelectorAll(".option");
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+
+function optionSelected(answer){
+    let userAns = answer.textContent;
+    let correcAns = questions[que_count].answer; 
+    var allOptions = option_list.children.length; 
+    if(userAns == correcAns){ 
+        userScore += 1; 
+        answer.classList.add("correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIconTag); 
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect"); 
+        answer.insertAdjacentHTML("beforeend", crossIconTag); 
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){  
+                option_list.children[i].setAttribute("class", "option correct"); 
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); 
+    }
+    next_button.classList.add("show"); 
+}
+
 
 function startTimer(time){
     counter = setInterval(timer, 1000);
